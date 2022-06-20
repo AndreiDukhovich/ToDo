8985f43825bdac8b452ff97826385f6552284922
AndreiDukhovich <andreidyhovich@gmail.com>
2022-06-20 15:10:50 +0300

    Change for deploy

--- ToDo/settings.py
+++ ToDo/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['django-insecure-hw(pg8*43$mbdxg4oe%&l*n^^+ze3+6bneobv0c9nfqn@pcgrx']
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'pg8*43$mbdxg4oe%&l*n^^+ze3+6bneobv0c9nfqn@pcgrx')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
